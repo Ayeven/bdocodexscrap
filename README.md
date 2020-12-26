25b5753281b6e7473a5dee42d1c4fd069be65b79
ayeven <dihi.mel@gmail.com>
2020-12-26 20:52:22 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple example to scrap BDOCodex page.
+I Commented quite a few what is what important lines
+
+You will need nodejs ver. 12 and above
+
+Feel Free to fork and add more feature
+
+const url = `https://bdocodex.com/de/skills/tamer/`;
+ - You can change the language `de` to another language based on your preference. So is `Tamer` to another class. This is for discord bot uses.
+ - You MIGHT want to change the element selector if you decided to change to another language.This is especially true when using `const url = https://bdocodex.com/us/search/Morning Star` search function.
--- search.js
+++ search.js
@@ -1,40 +1,39 @@
 
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 const cheerio = require ('cheerio');
 const base = 'https://bdocodex.com';
-const url = `https://bdocodex.com/us/search/Milk`;
+const url = `https://bdocodex.com/us/search/Morning Star`;
 
-   const a = puppeteer.launch({headless:true, slowMo:200,args: ['--no-sandbox', '--disable-setuid-sandbox'] }) // Change headless to true so browser didn't launch
-   .then(browser => browser.newPage())
-   .then(async page => {await page.goto(url)
-                //await page.select("select.form-control.input-sm","10") // Drop down how many data to show (10,25,50,100,150,200)
-                await page.click("input#SearchTable_object_1.dt_qfilter") // Item Checkbox
-                //await page.click("input#SearchTable_object_2.dt_qfilter") // NPC Checkbox
-                //await page.click("input#SearchTable_object_3.dt_qfilter") // Quest Checkbox
-                //await page.click("input#SearchTable_object_4.dt_qfilter") // Skill Checkbox
-                //await page.click("input#SearchTable_object_5.dt_qfilter") // Worker Skill
-                //await page.click("input#SearchTable_object_6.dt_qfilter") // Design
-                //await page.click("input#SearchTable_object_7.dt_qfilter") // Recipe
-                //await page.click("input#SearchTable_object_8.dt_qfilter") // Achievement
-                //await page.click("input#SearchTable_object_9.dt_qfilter") // Knowledge
-                //await page.click("input#SearchTable_object_10.dt_qfilter") // Guild Quest
-                //await page.click("input#SearchTable_object_11.dt_qfilter") // Recipe (Processing)
-                //await page.click("input#SearchTable_object_12.dt_qfilter") // Title
-                //await page.click("input#SearchTable_object_13.dt_qfilter") // Pet
-                //await page.click("input#SearchTable_object_14.dt_qfilter") // Mount
-                //await page.click("input#SearchTable_object_15.dt_qfilter") // Gatherable Object
-                //await page.click("input#SearchTable_object_16.dt_qfilter") // Node 
-                //await page.type("input.form-control.input-sm", "Material") // Filter Input textbox
-                return await page.content()
-    })
-        .then(html => {
-       const collect =[];
-       const $ = cheerio.load(html);
-       $('tbody>tr').each(function(){
-        const id = $(this).find('.dt-id').text();
-        const name = $(this).find('.dt-title.sorting_1').text();
-        const link = base + $(this).find('.iconset_wrapper_big>a').attr('href');
-        collect.push({id:id, name:name, link:link});
-       })
-       console.log(collect);
-   })
+puppeteer.launch({headless:false, slowMo:300,args: ['--no-sandbox', '--disable-setuid-sandbox'] }) // Change headless to true so browser didn't launch. --no-sandbox argument for sudo users
+  .then(browser => browser.newPage())
+  .then(async page => {await page.goto(url);
+    await page.select("select.form-control.input-sm","150"); // Drop down how many data to show (10,25,50,100,150,200)
+    await page.click("input#SearchTable_object_1.dt_qfilter"); // Item Checkbox
+    //await page.click("input#SearchTable_object_2.dt_qfilter") // NPC Checkbox
+    //await page.click("input#SearchTable_object_3.dt_qfilter") // Quest Checkbox
+    //await page.click("input#SearchTable_object_4.dt_qfilter") // Skill Checkbox
+    //await page.click("input#SearchTable_object_5.dt_qfilter") // Worker Skill
+    //await page.click("input#SearchTable_object_6.dt_qfilter") // Design
+    //await page.click("input#SearchTable_object_7.dt_qfilter") // Recipe
+    //await page.click("input#SearchTable_object_8.dt_qfilter") // Achievement
+    //await page.click("input#SearchTable_object_9.dt_qfilter") // Knowledge
+    //await page.click("input#SearchTable_object_10.dt_qfilter") // Guild Quest
+    //await page.click("input#SearchTable_object_11.dt_qfilter") // Recipe (Processing)
+    //await page.click("input#SearchTable_object_12.dt_qfilter") // Title
+    //await page.click("input#SearchTable_object_13.dt_qfilter") // Pet
+    //await page.click("input#SearchTable_object_14.dt_qfilter") // Mount
+    //await page.click("input#SearchTable_object_15.dt_qfilter") // Gatherable Object
+    //await page.click("input#SearchTable_object_16.dt_qfilter") // Node
+    return await page.content();
+  })
+  .then(html => {
+    const collect =[];
+    const $ = cheerio.load(html);
+    $('tbody>tr').each(function() {
+      const id = $(this).find('.dt-id').text();
+      const name = $(this).find('.dt-title.sorting_1').text();
+      const link = base + $(this).find('.iconset_wrapper_big>a').attr('href');
+      collect.push({id:id, name:name, link:link}); //Data Constructor
+    });
+    console.log(collect);
+  });

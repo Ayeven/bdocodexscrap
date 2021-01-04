d2dabcc864017bd3fc8e217f591bfe7c20f4a26d
Ayeven <dihi.mel@gmail.com>
2021-01-04 06:31:53 +0000

    added design_us

---
+++ wip/design_us.js
@@ -0,0 +1,52 @@
+
+const axios = require ('axios');
+const {decode} = require('html-entities');
+const lang = 'us';
+const itemID = '8847';
+const cheerio = require ('cheerio');
+
+axios(`https://bdocodex.com/tip.php?id=design--${itemID}&enchant=0&l=${lang}`, {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-requested-with": "XMLHttpRequest"
+  },
+  "referrer": "https://bdocodex.com/us/designs/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+}).then(file =>{
+  const result = [];
+  const f = file.data;
+  const $ = cheerio.load(f);
+  const name = $('#item_name').eq(0).text().trim();
+  const type = $('.category_text').text();
+  const req1 = $('td').eq(4).text().trim();
+  const qtty1 = $('a.qtooltip').eq(0).text().trim().padStart(1,1);
+  const mats1 = $('a.qtooltip').eq(1).text().trim().padStart(1,1);
+  const qtty2 = $('a.qtooltip').eq(2).text().trim().padStart(1,1);
+  const mats2 = $('a.qtooltip').eq(3).text().trim().padStart(1,1);
+  const qtty3 = $('a.qtooltip').eq(4).text().trim().padStart(1,1);
+  const mats3 = $('a.qtooltip').eq(5).text().trim().padStart(1,1);
+  const qtty4 = $('a.qtooltip').eq(6).text().trim().padStart(1,1);
+  const mats4 = $('a.qtooltip').eq(7).text().trim().padStart(1,1);
+  const qtty5 = $('a.qtooltip').eq(8).text().trim().padStart(1,1);
+  const mats5 = $('a.qtooltip').eq(9).text().trim().padStart(1,1);
+  const qtty6 = $('a.qtooltip').eq(10).text().trim().padStart(1,1);
+  const mats6 = $('a.qtooltip').eq(11).text().trim().padStart(1,1);
+  if((mats3 == name) ) { result.push({type:type,name:name,req1:req1, qtty1:qtty1,mats1:mats1,qtty2:qtty2,mats2:mats2});
+  console.log(result);} // 2 materials
+  else if((mats4 == name) ) { result.push({type:type,name:name,req1:req1, qtty1:qtty1,mats1:mats1,qtty2:qtty2,mats2:mats2, qtty3:qtty3,mats3:mats3});
+  console.log(result);} // 3 materials
+  else if((mats5 == name) ) { result.push({type:type,name:name,req1:req1, qtty1:qtty1,mats1:mats1,qtty2:qtty2,mats2:mats2, qtty3:qtty3,mats3:mats3,qtty4:qtty4,mats4:mats4});
+  console.log(result);} // 4 materials
+  else if((mats6 == name) ) { result.push({type:type,name:name,req1:req1, qtty1:qtty1,mats1:mats1,qtty2:qtty2,mats2:mats2, qtty3:qtty3,mats3:mats3,qtty4:qtty4,mats4:mats4,qtty5:qtty5,mats5:mats5});
+  console.log(result);} // 5 materials
+  else { result.push({type:type,name:name,req1:req1, qtty1:qtty1,mats1:mats1,qtty2:qtty2,mats2:mats2, qtty3:qtty3,mats3:mats3,qtty4:qtty4,mats4:mats4,qtty5:qtty5,mats5:mats5,qtty6:qtty6,mats6:mats6});
+  console.log(result);} // 6 materials
+});
--- wip/recipes_us.js
+++ wip/recipes_us.js
@@ -1,8 +1,8 @@
 
 const axios = require ('axios');
 const {decode} = require('html-entities');//eslint no-unused-vars
-const lang = 'us';
-const recipeID = '211';
+const lang = 'tw';
+const recipeID = '227';
 const cheerio = require ('cheerio');
 
 axios(`https://bdocodex.com/tip.php?id=recipe--${recipeID}&l=${lang}`, {
@@ -37,7 +37,11 @@ axios(`https://bdocodex.com/tip.php?id=recipe--${recipeID}&l=${lang}`, {
   const mats4 = $('a.qtooltip').slice(0).eq(7).text().trim().padStart(1,1);
   const qtty5 = $('a.qtooltip').slice(0).eq(8).text().trim().padStart(1,1);
   const mats5 = $('a.qtooltip').slice(0).eq(9).text().trim().padStart(1,1);
-  result.push({lvl:lvl,category:category, name:name, mats1:mats1,qtty1:qtty1,mats2:mats2,qtty2:qtty2,mats3:mats3,qtty3:qtty3,mats4:mats4,qtty4:qtty4,mats5:mats5,qtty5:qtty5});
-  console.log(result);
+  if ((mats4==name ) ){result.push({lvl:lvl,category:category, name:name, mats1:mats1,qtty1:qtty1,mats2:mats2,qtty2:qtty2,mats3:mats3,qtty3:qtty3});
+  console.log(result);} // 3 items materials
+  else if ((mats5==name ) ){result.push({lvl:lvl,category:category, name:name, mats1:mats1,qtty1:qtty1,mats2:mats2,qtty2:qtty2,mats3:mats3,qtty3:qtty3,mats4:mats4,qtty4:qtty4});
+  console.log(result);} // 4 items materials
+  else {result.push({lvl:lvl,category:category, name:name, mats1:mats1,qtty1:qtty1,mats2:mats2,qtty2:qtty2,mats3:mats3,qtty3:qtty3,mats4:mats4,qtty4:qtty4,mats5:mats5,qtty5:qtty5});
+  console.log(result);} // 5 items materials
 });
 //Recipes for Alchemy and Cooking
